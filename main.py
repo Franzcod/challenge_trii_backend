84896fc1e29673374519dc15bec3a3ec4d102675
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2022-02-21 18:32:56 -0300

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+import requests
+
+if __name__ == '__main__':
+    
+    stocks = ['AAPL','GOOGL','AMZN','TSLA','FB','TWTR','UBER','LYFT','SNAP','SHOP']
+    
+    listPrices = []
+    
+    
+    for stock in stocks:
+        url = f'https://financialmodelingprep.com/api/v3/quote-short/{stock}?apikey=c13a5d2ecf7cc6b8c50c06d7e1dfce22'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            listPrices.append(data[0]['price'])
+    
+    
+    
+    print('lista => ',listPrices)       
+    
+    
+    
+    
+
+    
+    
+    
